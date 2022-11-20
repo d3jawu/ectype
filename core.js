aed7754b88d75f286b3cb99acef0e37323c7197e
Holly <holly@dejawu.me>
2022-11-20 03:34:51 -0500

    Wrapping up this experiment

--- core.js
+++ core.js
@@ -28,7 +28,11 @@ const _incomplete = (completion, val) => {
 // `as` creates a shallow copy of `val` with its type set to `type`.
 // It does not perform any runtime type-checking.
 const as = (type, val) =>
-  Object.defineProperty({ ...val }, "__type__", { value: type });
+  ({
+    object: () =>
+      Object.defineProperty({ ...val }, "__type__", { value: type }),
+    function: () => Object.defineProperty(val, "__type__", { value: type }),
+  }[typeof val]());
 
 // These Untyped types are used to represent types of incoming untyped values from JS,
 // about which no guarantees can be made. Mainly used at app boundaries.
@@ -40,72 +44,77 @@ const UntypedArray = {};
 // and then filled out later. We set their keys to undefined now, so we can check
 // later that all of them have been properly filled in.
 // (An undefined key is not the same as an unset key; it appears during enumeration.)
-const ObjectMapType = _incomplete("ObjectMap", {
-  fields: undefined,
-  __type__: undefined,
-});
-const Type = _incomplete("Type", {
-  fields: undefined,
-  __type__: undefined,
-});
-_typed(Type, Type);
 
 // hard-coded Type for an ObjectMap that contains Types
 // this is a very important type, because it describes the `fields` key that all types use
 // to describe the values they create.
 // We have to be careful to only use `ObjectMapOfType.of` during initialization, and not allow
 // it to be used afterwards, because it creates incomplete values.
-const IncompleteObjectMapOfType = _typed(ObjectMapType, {
-  fields: _incomplete("ObjectMapOfTypeFields", {}),
-  contains: Type,
-  of: (obj) =>
-    _incomplete("ObjectMapOfTypeConstructor", {
+
+const ObjectMapOfType = _incomplete("ObjectMapOfTypeConstructor", {});
+const Type = ObjectMapOfType; // alias
+
+const incompleteObjectMapOfType = (obj) =>
+  _incomplete(
+    "ObjectMapOfTypeConstructor",
+    as(ObjectMapOfType, {
       obj,
-      has: undefined,
-      get: undefined,
-      set: undefined,
-      keys: undefined,
-      values: undefined,
-      __type__: undefined,
-    }),
+      // has: undefined,
+      get: undefined, // map function implementations will go here.
+      // set: undefined,
+      // keys: undefined,
+      // values: undefined,
+      // eq: undefined,
+    })
+  );
+
+_update(
+  ObjectMapOfType,
+  incompleteObjectMapOfType(
+    _incomplete("ObjectMapOfTypeType", {
+      // obj is not exposed on the type, it is used internally
+      // has: undefined,
+      get: undefined, // map function types will go here.
+      // set: undefined,
+      // keys: undefined,
+      // values: undefined,
+      // eq: undefined,
+    })
+  )
+);
+
+const Str = _incomplete("Str", incompleteObjectMapOfType({}));
+
+const Bool = _incomplete("Bool", incompleteObjectMapOfType({}));
+
+const FnFromStrToType = incompleteObjectMapOfType({
+  param: Str,
+  returns: Type,
 });
 
-// Type is the root self-describing type.
-_update(Type, {
-  fields: IncompleteObjectMapOfType.of({
-    fields: IncompleteObjectMapOfType,
-  }),
-});
+// clean up incompletes
+_incompletes
+  .filter(({ completion }) => completion === "ObjectMapOfTypeType")
+  .forEach(({ val }) => {
+    _update(val, {
+      get: FnFromStrToType,
+    });
+  });
 
-_update(ObjectMapType, {
-  fields: IncompleteObjectMapOfType.of({
-    fields: IncompleteObjectMapOfType,
-    contains: Type,
-  }),
-});
+_incompletes
+  .filter(({ completion }) => completion === "ObjectMapOfTypeConstructor")
+  .forEach(({ val }) => {
+    _update(val, {
+      get: as(FnFromStrToType, (key) => val.obj[key]),
+    });
+  });
+
+console.log(_incompletes);
+console.log(ObjectMapOfType.get("get"));
+console.log(ObjectMapOfType.__type__);
 
 // a type with no fields, that anything can be cast to.
-const Any = _typed(Type, {
-  fields: IncompleteObjectMapOfType.of({}),
-});
-
-const FnType = _typed(Type, {
-  fields: IncompleteObjectMapOfType.of({
-    fields: IncompleteObjectMapOfType,
-    param: Type,
-    returns: Type,
-  }),
-  // incomplete: `of` needs to be a typed function
-  // and it needs its Incomplete call
-  of: (param, returns) => {
-    return as(FnType, {
-      fields: IncompleteObjectMapOfType.of({}),
-      param,
-      returns,
-    });
-  },
-});
-
+// const Any = objectMapOfType({});
 // bootstrapping boundary: at this point Type, ObjectMap, Fn are complete and all their features may be used safely without caveat.
 
 const typeOf = (val) =>
@@ -125,7 +134,4 @@ const typeOf = (val) =>
     },
   }[typeof val]());
 
-console.log(_incompletes);
-console.log(FnType.of(Type, Type));
-
-export { Type, ObjectMapType as ObjectMap, Any };
+// console.log(_incompletes);
