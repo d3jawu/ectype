ec85f7c956dbc72e4c1e97287bc8519c046ec85c
Holly <holly@dejawu.me>
2022-12-05 02:58:29 -0500

    Move some stuff around

--- lib/core/types.ts
+++ lib/core/types.ts
@@ -9,8 +9,18 @@ export type Type =
   | StructType
   | TypeType;
 
-// `sub` returns true if this type is a subtype of `other` - that is,
-// it can be safely used anywhere `other` is used.
+// `from` is a passthrough function used to signal to the type-checker
+// that the value being passed in is expected to verifiably fulfill
+// the type at compile-time.
+
+// `conform` is a function that receives any value and returns an Option
+// containing the value if it is valid (see `valid` below), or None if not.
+
+// `valid` receives any value and returns true if it is a valid instance of
+// the type, false otherwise.
+
+// `sub` returns true if the type is a subtype of `other` - that is, values of
+// the type can be safely substituted in anywhere a value of `other` is present.
 
 export interface NullType {
   from: (val: null) => null;
--- lib/stdlib/stdlib.ts
+++ lib/stdlib/stdlib.ts
@@ -1,7 +1,7 @@
 // stdlib.js contains library functions implemented entirely in Kythera user-space.
-import { variant } from "./core.js";
+import { variant } from "../core/core.js";
 
-import { Null } from "./primitives.js";
+import { Null } from "../core/primitives.js";
 
 // TODO check that T is a Type
 const option = (T) => variant({ Some: T, None: Null });
