6a73c76771db42ffabca674e2afcece5f6448d45
Holly <holly@dejawu.me>
2023-03-01 21:43:46 -0500

    Fix test

--- test/tests/array.ts
+++ test/tests/array.ts
@@ -2,6 +2,8 @@ import { array } from "../../core/array.js";
 import { struct } from "../../core/struct.js";
 import { Num } from "../../core/primitives.js";
 
+import { someOf } from "../../core/util.js";
+
 import { strict as assert } from "node:assert";
 
 export function conform() {
@@ -9,9 +11,7 @@ export function conform() {
 
   const numArray = NumArray.conform([1, 2, 3]);
 
-  assert.deepEqual(numArray, {
-    Some: [1, 2, 3],
-  });
+  assert.deepEqual(JSON.stringify(numArray), JSON.stringify(someOf([1, 2, 3])));
 }
 
 export function subtype() {
