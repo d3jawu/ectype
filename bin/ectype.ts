d574891d82ad0fb3a4a6905b688f6dcaf5f4db46
Holly <holly@dejawu.me>
2023-10-16 06:53:52 -0400

    Resolve line and column numbers from error spans

--- bin/ectype.ts
+++ bin/ectype.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
-import { resolve } from "node:path";
+import { dirname, relative, resolve } from "node:path";
 import { analyzeFile } from "./analyze/analyzeFile.js";
+
+import { Span } from "@swc/core";
+import { readFileSync } from "node:fs";
 
 if (!process.argv[2] || process.argv[2] !== "check") {
   console.log(`Usage: ectype check entrypoint.js`);
@@ -15,6 +18,40 @@ if (!process.argv[3]) {
 
 const entryPoint = resolve(process.argv[3]);
 
+const spanToPos = (span: Span, sourceCode: string) => {
+  // Span seems to start at 1, subtract to offset this.
+  const startOffset = Math.min(span.start, sourceCode.length) - 1;
+  const endOffset = Math.min(span.end, sourceCode.length) - 1;
+
+  let startLine = 1;
+  let startColumn = 1;
+  let endLine = 1;
+  let endColumn = 1;
+
+  for (let i = 0; i < startOffset; i++) {
+    if (sourceCode[i] === "\n") {
+      startLine++;
+      startColumn = 1;
+    } else {
+      startColumn++;
+    }
+  }
+
+  for (let i = 0; i < endOffset; i++) {
+    if (sourceCode[i] === "\n") {
+      endLine++;
+      endColumn = 1;
+    } else {
+      endColumn++;
+    }
+  }
+
+  return {
+    start: { line: startLine, column: startColumn },
+    end: { line: endLine, column: endColumn },
+  };
+};
+
 try {
   const res = analyzeFile(entryPoint);
   if (res === null) {
@@ -24,7 +61,29 @@ try {
 
   const { errors, warnings } = res;
 
-  console.log(errors);
+  let errored = false;
+  Object.entries(errors).forEach(([path, errors]) => {
+    if (errors.length === 0) {
+      return;
+    }
+
+    errored = true;
+    const file = readFileSync(path).toString();
+
+    errors.forEach(({ span, message }) => {
+      const { start, end } = spanToPos(span, file);
+
+      process.stdout.write(
+        `${relative(dirname(entryPoint), path)}:${start.line}:${
+          start.column
+        }: ${message}\n`
+      );
+    });
+  });
+
+  if (errored) {
+    process.exit(1);
+  }
 } catch (e: any) {
   console.log(`Error: ${e.message}`);
   process.exit(1);
